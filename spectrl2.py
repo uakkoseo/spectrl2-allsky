d4cea8c166f3ce0c5e1bb87653a19808afbb5a2e
Akkoseoglu <Umay.Akkoseoglu@dnv.com>
2025-10-01 19:15:19 -0700

    spectrl2_boulder_test

--- spectrl2.py
+++ spectrl2.py
@@ -1,22 +1,29 @@
+import pvlib as pvl
+import pandas as pd
+import glob
+
+
 def this_site_info(loc, site_info):
-        this_site_info = site_info.loc[loc]
-        site = pvl.location.Location(
-                latitude=this_site_info['Latitude']
-                , longitude=this_site_info['Longitude']
-                , tz=this_site_info['local_tz']
-        , altitude=this_site_info['Elevation']
+    this_site_info = site_info.loc[loc]
+    site = pvl.location.Location(
+             latitude=this_site_info['Latitude'],
+             longitude=this_site_info['Longitude'],
+             tz=this_site_info['local_tz'],
+             altitude=this_site_info['Elevation']
         )
-        return site
+    return site
 
 
 def site_arguments(spectral_path, loc, dtadir):
     '''
-    Function to create arguments based on weather data'
+    Function to create arguments based on weather data
     '''
-    solcast_tmy = glob.glob(pathname='*.csv', root_dir= spectral_path / loc / 'Resource/Solcast')[0]
+    solcast_tmy = glob.glob(
+        pathname='*.csv', root_dir= spectral_path / loc / 'Resource/Solcast'
+        )[0]
     tmy_data_path = spectral_path / loc / 'Resource/Solcast' / solcast_tmy
     tmy = pd.read_csv(tmy_data_path, index_col=0, parse_dates=True)
-    tmy_pressure = tmy['surface_pressure']*100 
+    tmy_pressure = tmy['surface_pressure']*100
     # converting solcast pwat units to cm
     tmy_pwat = tmy['precipitable_water']/997*100
     tmy_zenith = tmy['zenith']
@@ -24,17 +31,20 @@ def site_arguments(spectral_path, loc, dtadir):
 
     return tmy, tmy_pressure, tmy_pwat, tmy_zenith, tmy_times
 
+
 def spectral2_arguments(tmy, tmy_solpos, tracker_config):
-    tracker_angles = pvl.tracking.singleaxis(tmy_solpos.apparent_zenith,
-                                         tmy.azimuth,
-                                         tracker_config['axis_tilt'],
-                                         tracker_config['axis_azimuth'],
-                                         tracker_config['max_tracker_angle'],
-                                         tracker_config['backtrack'],
-                                         tracker_config['gcr'])
+    tracker_angles = pvl.tracking.singleaxis(
+        tmy_solpos.apparent_zenith,
+        tmy.azimuth,
+        tracker_config['axis_tilt'],
+        tracker_config['axis_azimuth'],
+        tracker_config['max_tracker_angle'],
+        tracker_config['backtrack'],
+        tracker_config['module_length']/tracker_config['pitch'])
     tmy_tilt = tracker_angles.tracker_theta
     tmy_aoi = tracker_angles.aoi
-    tmy_relhum = pvl.atmosphere.get_relative_airmass(tmy_solpos.apparent_zenith,
-                                                   model='kastenyoung1989')
+    tmy_relhum = pvl.atmosphere.get_relative_airmass(
+         tmy_solpos.apparent_zenith, model='kastenyoung1989'
+                                                   )
 
     return tracker_angles, tmy_tilt, tmy_aoi, tmy_relhum
